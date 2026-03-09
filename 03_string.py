a2772d2b140ac41572bd0ec8177b21d593e5028b
김준한 <k0j4h25@hanmail.net>
2026-03-09 14:10:33 +0900

    첫 커밋

---
+++ 03_string.py
@@ -0,0 +1,11 @@
+print("문자열")
+print(type("문자열"))
+print(type("1"))
+print(2+3)
+print("2"+"3")
+# 에러
+# print("2"+3) - 문자열과 정수형의 합은 Type Error
+print(type(""))
+print(type("    "))
+print("하이"+" 난 김준한")
+print("준한"*10)
